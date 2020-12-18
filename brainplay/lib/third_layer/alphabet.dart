70552707b9738e500b91329a94173699f32fd5b8
stevendevilla <stevendevilla9@gmail.com>
2020-12-19 00:16:35 +0100

    carousel for numbers and alphabet capital and small letters completed,

--- brainplay/lib/third_layer/alphabet.dart
+++ brainplay/lib/third_layer/alphabet.dart
@@ -1,24 +1,22 @@
 import 'package:brainplay/functions.dart';
 import 'package:flutter/material.dart';
 import 'package:carousel_images/carousel_images.dart';
+import 'package:buttons_tabbar/buttons_tabbar.dart';
 
 class Alphabet extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      home: AlphaPage(),
     );
   }
 }
 
-class HomePage extends StatelessWidget {
-  final List<String> listImages = [
-    'assets/letters/A.gif',
-    'assets/letters/B.gif',
-    'assets/letters/C.gif',
-    'assets/letters/D.gif',
-    'assets/letters/E.gif'
-  ];
+class AlphaPage extends StatelessWidget {
+  //65-91 Capital
+  //97-123 Small
+  final capitalLetters = letters(start: 65, end: 91).cast<String>().toList();
+  final smallLetters = letters(start: 97, end: 123).cast<String>().toList();
 
   @override
   Widget build(BuildContext context) {
@@ -31,22 +29,77 @@ class HomePage extends StatelessWidget {
             Color(0xFFFE6D73),
             Colors.white,
           ),
-          child: Column(
-            children: <Widget>[
-              SizedBox(height: 100),
-              CarouselImages(
-                scaleFactor: 0.3,
-                listImages: listImages,
-                height: 400.0,
-                borderRadius: 50.0,
-                onTap: (index) {
-                  print('Tapped on page $index');
-                },
-              )
-            ],
+          child: Container(
+            child: DefaultTabController(
+              length: 2,
+              child: Column(
+                children: <Widget>[
+                  ButtonsTabBar(
+                    backgroundColor: Colors.blue[600],
+                    unselectedBackgroundColor: Colors.white,
+                    labelStyle: TextStyle(
+                        color: Colors.white, fontWeight: FontWeight.bold),
+                    unselectedLabelStyle: TextStyle(
+                        color: Colors.blue[600], fontWeight: FontWeight.bold),
+                    borderWidth: 1,
+                    unselectedBorderColor: Colors.blue[600],
+                    radius: 100,
+                    tabs: [
+                      Tab(
+                        //icon: Icon(Icons.directions_car),
+                        text: "ABC",
+                      ),
+                      Tab(
+                        //icon: Icon(Icons.directions_transit),
+                        text: "abc",
+                      ),
+                    ],
+                  ),
+                  Expanded(
+                    child: TabBarView(
+                      children: <Widget>[
+                        CarouselImages(
+                          scaleFactor: 0.5,
+                          listImages: capitalLetters,
+                          height: 400.0,
+                          borderRadius: 50.0,
+                          onTap: (index) {
+                            print('Tapped on page $index');
+                          },
+                        ),
+                        CarouselImages(
+                          scaleFactor: 0.5,
+                          listImages: smallLetters,
+                          height: 400.0,
+                          borderRadius: 50.0,
+                          onTap: (index) {
+                            print('Tapped on page $index');
+                          },
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ),
         ),
       ),
     );
   }
+}
+
+List<dynamic> letters({int start, int end}) {
+  List myList = [];
+  for (int i = start; i < end; i++) {
+    String letter = String.fromCharCode(i);
+    if (end < 92) {
+      String location = "assets/capital_letters/$letter.gif";
+      myList.add(location);
+    } else {
+      String location = "assets/small_letters/$letter.gif";
+      myList.add(location);
+    }
+  }
+  return myList;
 }
--- brainplay/lib/third_layer/numbers.dart
+++ brainplay/lib/third_layer/numbers.dart
@@ -1,15 +1,52 @@
-import 'package:flutter/material.dart';
 import 'package:brainplay/functions.dart';
+import 'package:flutter/material.dart';
+import 'package:carousel_images/carousel_images.dart';
 
 class Numbers extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: myAppBar(),
-      body: Container(
-        decoration: gradient(Color(0xFFFE6D73), Colors.white),
-        child: Text('Numbers!'),
+    return MaterialApp(
+      home: NumPage(),
+    );
+  }
+}
+
+class NumPage extends StatelessWidget {
+  final numberList = numbers().cast<String>().toList();
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Scaffold(
+        appBar: myAppBar(),
+        body: Container(
+          decoration: gradient(Colors.red, Colors.white),
+          child: Column(
+            children: <Widget>[
+              SizedBox(height: 100),
+              CarouselImages(
+                scaleFactor: 0.7,
+                listImages: numberList,
+                height: 300.0,
+                borderRadius: 30.0,
+                verticalAlignment: Alignment.bottomCenter,
+                onTap: (index) {
+                  print('Tapped on page $index');
+                },
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
+}
+
+List<dynamic> numbers() {
+  List myList = [];
+  for (int i = 1; i < 11; i++) {
+    String location = "assets/numbers/$i.gif";
+    myList.add(location);
+  }
+  return myList;
 }
